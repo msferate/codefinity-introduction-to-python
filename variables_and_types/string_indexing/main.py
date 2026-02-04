56ea763e1798b7220bb36c182275f5b027866144
msferate <>
2026-02-04 06:27:14 +0000

    String Indexing

---
+++ variables_and_types/string_indexing/main.py
@@ -0,0 +1,17 @@
+grocery_item = "Grilled Chicken Salad"
+
+# Find the length of the string, which includes spaces
+length_of_item = len(grocery_item) # Includes spaces in the count
+
+# Acessing the first and last charater using indexing
+first_char = grocery_item[0]
+second_char = grocery_item[8]
+third_char = grocery_item [16]
+last_char1 = grocery_item[-15]
+last_char2 = grocery_item[-7]
+last_char3 = grocery_item[-1]
+
+# Testing
+print("Length of item name:", length_of_item)
+print("First character of each word:", first_char, second_char, third_char)
+print("Last character of each word:", last_char1, last_char2, last_char3)
